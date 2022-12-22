3ca0b50432fe276c30e77ef31db36f2ca23e3968
Andrew Zhuravlev <zhura2296@yandex.ru>
2022-12-22 13:23:56 +0300

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('level-1')
+const express = require('express');
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+    // Если всё работает, консоль покажет, какой порт приложение слушает
+    console.log(`App listening on port ${PORT}`)
+})
