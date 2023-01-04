ea60af3085ce61e0aea1d05d28d68fac3a458b15
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-01-04 15:49:34 +0300

    fix: hash

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,22 @@
+const bcrypt = require('bcryptjs'); // импортируем bcrypt
 const Auth = require('../models/user');
 
 module.exports.createUsers = (req, res) => {
 
-  const { email, password, name } = req.body;
-
-  Auth.create({ email, password, name })
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => {
+      Auth.create({
+        email: req.body.email,
+        password: hash,
+        name:req.body.name
+      })
+    })
     .then((user) => {
-      res.send({ data: user })
+      res.send({
+         email: user.email,
+         name: user.name,
+         _id: user._id,
+       })
     })
     .catch((err) => {
       res.status(500).send({ message: err })
