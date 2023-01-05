7a4c6ce4b27f8dfe1bd04a55358bcb7a6040164f
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-01-05 15:47:32 +0300

    fix: middlewares/auth

---
+++ constants/constants.js
@@ -0,0 +1,6 @@
+/// коды ошибок
+module.exports.SALT_ROUND = 10;
+/// jwt
+module.exports.SECRET_KEY_JWT = 'some-secret-key';
+
+module.exports.REGEX = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/;
