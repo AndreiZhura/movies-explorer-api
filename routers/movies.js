6fa19a854a73acc6005e996eb43584dcbfdfa550
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-01-06 12:48:49 +0300

    fix: Joi

--- routers/movies.js
+++ routers/movies.js
@@ -1,10 +1,30 @@
 
 const movieRouters = require('express').Router();
-
+const REGEX = require('../constants/constants');
 const { createMovies, getMovie, deleteMovie } = require('../controllers/movies');
 
 movieRouters.get('/movies', getMovie);
-movieRouters.post('/movies', createMovies);
-movieRouters.delete('/movies/:_id', deleteMovie);
+movieRouters.post('/movies',celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.number().required().min(4).max(4),
+    description: Joi.string().required(),
+    image: Joi.string().required(REGEX),
+    trailerLink: Joi.string().required(REGEX),
+    thumbnail: Joi.string().required(REGEX),
+    movieId: Joi.string().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+}),
+ createMovies);
+movieRouters.delete('/movies/:_id',celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().hex().length(24),
+  }),
+}),
+ deleteMovie);
 
 module.exports = movieRouters;
--- routers/users.js
+++ routers/users.js
@@ -3,7 +3,13 @@ const userRouters = require('express').Router();
 const { updateUserMe, getUsersMe } = require('../controllers/users');
 
 userRouters.get('/users/me',getUsersMe)
-userRouters.patch('/users/me', updateUserMe)
+userRouters.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email(),
+    name: Joi.string().min(2).max(30),
+  }),
+}),
+updateUserMe)
 
 
 module.exports = userRouters;
