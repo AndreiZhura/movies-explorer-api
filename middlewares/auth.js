4049c64e17f7f16a257e98f4a318a88502d7a76a
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-01-07 22:46:11 +0300

    необходима авторизация

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,21 @@
 const jwt = require('jsonwebtoken');
+const { SECRET_KEY_JWT } = require('../constants/constants');
+const AuthorizationRequired = require('../errors/AuthorizationRequired');
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    throw new AuthorizationRequired('Необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, SECRET_KEY_JWT);
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new AuthorizationRequired('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
