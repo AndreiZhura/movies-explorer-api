457d86876662d79ed0bc4950daea567929e01165
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-01-11 16:50:34 +0300

    fix:проверка

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+// Mongoose - это инструмент моделирования объектов MongoDB,
+// предназначенный для работы в асинхронной среде. Мангуст поддерживает Node.js и Дено (альфа).
 const bodyParser = require('body-parser');
+// Сборка пакетов: body-parser
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+// const DATABASE_ADRESS = require('./constants/constants');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routers/index');
 const { apiRequestLimiter } = require('./riteLimited/riteLimited');
@@ -23,22 +27,18 @@ mongoose.connect(DATABASE_ADRESS);
 app.use(helmet());
 app.disable('x-powered-by');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // для собирания JSON-формата
+app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+/* Аргументом методу bodyParser.urlencoded мы передаём объект опций.
+ "extended: true" означает, что данные в полученном объекте body могут быть любых типов. */
 app.use(apiRequestLimiter);
-
 app.use('/', routes);
-
-app.use(errors());
-
-app.use(requestLogger);
-
-app.use(errorLogger);
-
-app.use(errors());
 
 app.use('*', NOT_FOUND_ERROR);
 
+app.use(requestLogger);
+app.use(errorLogger); // подключаем логгер ошибок
+app.use(errors()); // обработчик ошибок celebrate
 app.use(SERVER_ERROR);
 
 app.listen(PORT);
