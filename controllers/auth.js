36192c9b88b144f2b81fa67d960e821fad267a89
Andrew Zhuravlev <zhura2296@yandex.ru>
2023-02-22 13:39:34 +0300

    [200~        password: user.password,

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,6 +29,7 @@ module.exports.createUser = (req, res, next) => {
       res.status(201).send({
         email: user.email,
         name: user.name,
+        password: user.password,
         _id: user._id,
       });
     })
